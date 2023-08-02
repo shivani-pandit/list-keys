0dbf40e3fb2537a28eea9ea7300ab5e46ac3290f
shivani-pandit <shivanipandit562@gmail.com>
2023-08-02 16:57:08 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './Components/UserProfile/index'
+
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Esther Howard',
+  role: 'Software Developer',
+}
+
+const App = () => <UserProfile />
+
+export default App
---
+++ src/UserProfile/index.js
@@ -0,0 +1,6 @@
+const UserProfile = () => (
+  <div>
+    <img src="https://assets.ccbp.in/frontend/react-js/esther-howard-img.png" />
+  </div>
+)
+export default UserProfile
